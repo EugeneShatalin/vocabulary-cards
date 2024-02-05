9a6ad1d85cd78bcacb42cb02545d30112e48e4ab
Yevgeniy Shatalin <shatalin_ei@mail.ru>
2024-02-06 00:09:13 +0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterOutlet} from '@angular/router';
+import {VerbItem} from "../shared/models/verbItem";
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'vocabulary-cards';
+
+  tableHeaders = {
+    first: '1-ая форма',
+    second: '2-ая форма',
+    third: '3-ая форма',
+    translation: 'Перевод'
+  }
+
+  items: VerbItem[] = [
+    new VerbItem(
+      'be',
+      'was/were',
+      'been',
+      'быть, являться, находиться',
+      'Don\'t <b>be</b> late.',
+      'I <b>was</b> tired after work.',
+      'I\'ve never <b>been</b> this happy.',
+    ),
+    new VerbItem(
+      'do',
+      'did',
+      'done',
+      'делать, выполнять',
+      'What <b>do</b> you do? <br>\I can\'t <b>do',
+      'You <b>did</b> a great job!',
+      'I\'ve never <b>done</b> this before.',
+    ),
+    new VerbItem(
+      'go',
+      'went',
+      'gone',
+      'идти',
+      'I <b>go</b> to the store every other day',
+      'I <b>went</b> to the store yesterday.',
+      'Tom\'s not here. He\'s <b>gone</b> to the store.',
+    ),
+  ]
+
 }
---
+++ src/shared/models/verbItem.ts
@@ -0,0 +1,12 @@
+export class VerbItem {
+  constructor(
+    public firstFormVerb: string = '',
+    public secondFormVerb: string = '',
+    public thirdFormVerb: string = '',
+    public translation: string = '',
+    public firstExample: string = '',
+    public secondExample: string = '',
+    public thirdExample: string = '',
+  ){}
+}
+
